ad484c19698f5b198202807dea28678adc6c0d68
gagangayari <55296443+gagangayari@users.noreply.github.com>
2021-03-20 00:18:00 +0530

    Update ga.h

--- GA_in_C/ga.h
+++ GA_in_C/ga.h
@@ -1,17 +1,44 @@
-#define output_rows 10000
+#ifndef check_if_included
+#define check_if_included
+
+#include<time.h>
+#define output_rows 1000
 #define output_cols 100
 
-#define mut_prob 0.01
+#define mut_prob 0.3
 #define xover_prob 0.3
 
 #define key_size 32
-#define max_offsprings 300
+#define max_offsprings 400
 #define init_pop 400
 #define max_itr 100
 
+
+
 int found_flag;
-const int actual_output[output_rows][output_cols];
+int offsprings_count=0;
+int actual_output[output_rows][output_cols];
 int actual_input[output_rows][output_cols];
 int curr_output[output_rows][output_cols];
+
+
+/* ############# Structures #############*/
+
+
+struct chrom
+{
+    int key[key_size];
+    double fitness ;    
+    double error;
+};
+
+struct custom_struct{
+    int first,second;
+};
+
+
+#endif
+
+
 
 
