54d0ea9eb04a3dec439f360e110955012060a549
gagangayari <55296443+gagangayari@users.noreply.github.com>
2021-03-21 11:38:40 +0530

    Update ga.h

--- GA_in_C/ga.h
+++ GA_in_C/ga.h
@@ -2,6 +2,7 @@
 #define check_if_included
 
 #include<time.h>
+#include "hash_map.c"
 #define output_rows 1000
 #define output_cols 100
 
@@ -9,9 +10,9 @@
 #define xover_prob 0.3
 
 #define key_size 32
-#define max_offsprings 400
-#define init_pop 400
-#define max_itr 100
+#define max_offsprings 10
+#define init_pop 10
+#define max_itr 5
 
 
 
@@ -20,6 +21,8 @@ int offsprings_count=0;
 int actual_output[output_rows][output_cols];
 int actual_input[output_rows][output_cols];
 int curr_output[output_rows][output_cols];
+
+double average_error[max_itr];
 
 
 /* ############# Structures #############*/
