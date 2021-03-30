3372a0a5adf04a1e4bd09bc5318d17c03793a083
gagangayari <55296443+gagangayari@users.noreply.github.com>
2021-03-30 11:04:31 +0530

    Update ga_in_c.c

--- GA_in_C/ga_in_c.c
+++ GA_in_C/ga_in_c.c
@@ -8,88 +8,199 @@
 
 int main(){
 
+    srand(time(NULL));
+    clock_t t;
+    struct timespec t1,t2;
+    t1.tv_sec=0;
+    t1.tv_nsec=999999999;
 
-    // struct chrom a ={init(&a),-1};
-    
     //Initialise population
     struct chrom *pop=(struct chrom*) malloc(init_pop * (sizeof(struct chrom)));
     struct chrom *childs=(struct chrom*) malloc(max_offsprings * (sizeof(struct chrom)));
     struct chrom *combined=(struct chrom*) malloc((init_pop+max_offsprings) * (sizeof(struct chrom)));
-    struct Node *hash_map= NULL ;
+    struct Node *hash_map=NULL ;
     init_chrom(init_pop,pop);
-    fitness(pop,init_pop);
-    int mut_count=0;
+    hash_map=fitness(pop,init_pop,hash_map);
+    // disp_chrom_data(pop,init_pop);
+
+    if(found_flag==1){
+        printf("Found Exact Key :");
+        exit(0);
+    }
     
-    int n; //For representing binary keys into integers
+    FILE *file=fopen("avg_values.csv","w");
+
+    read_act_output(); //reads the actual output in actual_output
+
+    
+    int mut_count=0;
+
+
+    
+    unsigned int n1,n2,n; //For representing binary keys into integers
     struct custom_struct parents;
     // struct Node *map =NULL;
     int pop_size=init_pop;
-    
+    int continuity_count=0,prev=0;
+    int count[max_offsprings];
+    // mutate_prob(pop,init_pop,count);
+
+
     
     for( int i=0;i<max_itr;i++){
+        t=clock();
+
+
         average_error[i] =    avg_err(pop,init_pop);
 
-        printf("Itereation : %d . Average error : %lf\n",i,average_error[i]);
-        for( int j=0;j<max_offsprings;j++){
-        // printf("Offspring : %d \n",j);
+        //Checks if the error is stucked
+        if(i>0 && average_error[i-1]-average_error[i]<0.01){
+            // printf("err here : %lf",average_error[i]-average_error[i-1]);
+            if(prev==i-1){
+                if(continuity_count ==7){
+                    printf("Variation done ..! \n");
+                    vary_flag=1;
+                    variation(pop,init_pop,hash_map);
+                    average_error[i]=avg_err(pop,init_pop);
+                    continuity_count=0;
+                }
+                else{
+                    continuity_count++;
+                    prev=i;
+                }
+            }
+            else{
+                prev=i;
+            }
+        }
 
+        //Write the average errors into a csv file
+        if(i<max_itr-1){
+            fprintf(file,"%lf,",average_error[i]);
+
+        }
+        else{
+            fprintf(file,"%lf\n",average_error[i]);
+
+        }
+        printf("Itereation : %d . Average error : %lf\n",i,average_error[i]);
+
+        //Mutation using probabilities.Parents with less error will have more childs
+        mutate_prob(pop,max_offsprings,count);
+        int y=0,j=0,a,x=0;
+        while( y< max_offsprings){
+            a=0;
+            while(a!= count [j] ){
+                mutation(&pop[x],&childs[y]);
+                n=bin2int(childs[y].key,key_size);
+                while(search(n,hash_map)){
+                    mutation(&pop[x],&childs[y]);
+                    n=bin2int(childs[y].key,key_size);
+                }
+                y++;
+                a++;
+                
+            }
+            x++;
+            j++;
+
+            if(j==max_offsprings-1 && y<max_offsprings ){
+                j=0;
+            }
+        }
+
+        /*for( int j=0;j<max_offsprings;j+=2){
+
+            parents=selection(pop,max_offsprings,parents);
+
+            uniform_crossover(&pop[parents.first],&pop[parents.second],&childs[j],&childs[j+1]);
             
-            parents=selection(pop,max_offsprings);
-            // printf("Done selecetion %d,%d \n",parents.first,parents.second);
+            n1=bin2int(childs[j].key,key_size); //converting binary key to integer
+            n2=bin2int(childs[j+1].key,key_size); 
+
+            //check if the two childs are produced generated before
+            while(search(n1,hash_map) ){
+                uniform_crossover(&pop[parents.first],&pop[parents.second],&childs[j],&childs[j+1]);
+                n1=bin2int(childs[j].key,key_size); 
+                
+            }
+            while(search(n2,hash_map) && n1 !=n2){
+                uniform_crossover(&pop[parents.first],&pop[parents.second],&childs[j],&childs[j+1]);
+                n2=bin2int(childs[j+1].key,key_size);
+                
+            }
 
             mutation(&pop[parents.first],&childs[j]);
-            // printf("Done Mutation \n");
+            mutation(&pop[parents.second],&childs[j+1]);
 
-            n=bin2int(childs[j].key,key_size); //converting binary key to integer
-            disp_chrom_key(&childs[j],1);
-            // printf("Bin2int : %d\n",n);
+            n1=bin2int(childs[j].key,key_size); 
+            n2=bin2int(childs[j+1].key,key_size);
 
-            hash_map=insertNode(hash_map,n);
-            // printf("Hashmap \n");
+            while(search(n1,hash_map)){
+                mutation(&pop[parents.first],&childs[j]);
 
-            mut_count=0;
-            while(search(n,hash_map)){  //Checks if that key already generated before
-                mutation(&pop[j],&childs[j]);
-                n=bin2int(childs[j].key,key_size);
-                mut_count++;
-
+                n1=bin2int(childs[j].key,key_size); 
+                
             }
-            printf("Mutation %d\n",mut_count);
+            while(search(n2,hash_map) && n1!=n2){
+                mutation(&pop[parents.second],&childs[j+1]);
 
+                n2=bin2int(childs[j+1].key,key_size);
+                
+            }
+
+        // printf("Reached here %d\n",rand());
+
+            // nanosleep(&t1,&t2);
 
         }
-        // printf("Reached here \n");
-
-        fitness(childs,max_offsprings);
+        */
+        hash_map=fitness(childs,max_offsprings,hash_map);
+        if(found_flag==1){
+            printf(("Found exact key "));
+            fclose(file);
+            exit(0);
+        }
         fill_combined(pop,childs,combined);
-        bsort_samples(combined,max_offsprings+init_pop);
-        for(int k=0;k<init_pop;k++){
+        qsort(combined,max_offsprings+init_pop,sizeof(struct chrom),comparator);
+        for(int k=0;k<max_offsprings;k++){
             pop[k]=combined[k];
         }
+        
+        // printf("Elements in hashmap : %d\n",count_nodes(hash_map));
 
 
+        t=clock()-t;
+        printf("Time taken for iteration : %lf\n",((double) t)/CLOCKS_PER_SEC);
     } 
-    print_inorder(hash_map);
+        
+    
+    
+
+    printf("Elements in hashmap : %d\n",count_nodes(hash_map));
     
    /* int key[32]={1,1,0,1, 0,0,1,1, 0,1,1,0, 0,1,0,1, 0,0,1,0, 0,0,1,0,  0,1,1,0,  1,1,0,1};
     
-    read_act_output();
     for(int i=0;i<key_size;i++){
         pop[1].key[i]=key[i];
     }
     // mutation(&pop[1],&childs[0]);
-    fitness(pop,init_pop);
+    hash_map=fitness(pop,init_pop,hash_map);
     bsort_samples(pop,init_pop);
     if(found_flag){
         printf("Found \n");
+        exit(0);
     }
     // disp_chrom_key(&pop[1],1);
     disp_chrom_data(pop,init_pop);
 
-    // disp_chrom_key(&childs[0],1);
-    // disp_2d(curr_output,100,100);
-    
     */
+    fclose(file);
+   free(pop);
+   free(childs);
+
+   free(combined);
+   free (hash_map);
 
     return 0;
 
